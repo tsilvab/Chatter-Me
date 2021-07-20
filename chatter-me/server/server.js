dab7d9eba2b0f2bf573f2105c4a20247a8abf86c
Jesse Orozco <orozcojesse0@gmail.com>
2021-07-20 19:45:08 -0400

    starter code

--- chatter-me/server/server.js
+++ chatter-me/server/server.js
@@ -1,11 +1,16 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const path = require('path');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io").listen(server);
+const users = [];
+const connections = [];
 
-const { typeDefs, resolvers } = require('./schemas');
+const { typeDefs, resolvers } = require("./schemas");
 // Import `authMiddleware()` function to be configured with the Apollo Server
-const { authMiddleware } = require('./utils/auth');
-const db = require('./config/connection');
+const { authMiddleware } = require("./utils/auth");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -22,17 +27,54 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
+});
+
+// Route to our home page;
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// Connection with Socket.io;
+io.sockets.on("connection", function (socket) {
+  connections.push(socket);
+  console.log("Connected: %s sockets Connected", connections.length);
+
+  // Disconnect;
+  socket.on("disconnect", function (data) {
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsernames();
+    connections.splice(connections.indexOf(socket), 1);
+    console.log("Disconnected: %s sockets connected", connections.length);
+  });
+
+  // Send Message;
+  socket.on("send message", function (data) {
+    console.log(data);
+    io.sockets.emit("new message", { msg: data, user: socket.username });
+  });
+
+  // New User;
+  socket.on("new user", function (data, callback) {
+    callback(true);
+    socket.username = data;
+    users.push(socket.username);
+    updateUsernames();
+  });
+
+  function updateUsernames() {
+    io.sockets.emit("get users", users);
+  }
 });
